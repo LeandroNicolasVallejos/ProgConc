707a9a788b0590dac09fa5053b86cc2ec7a10cb9
Nicolás <Nicols@192.168.0.168>
2020-09-04 16:31:54 -0300

    los hilos ahora se crean con repetitivas

--- src/TP2/Ej3/prueba.java
+++ src/TP2/Ej3/prueba.java
@@ -11,22 +11,33 @@ package TP2.Ej3;
  */
 public class prueba {
     public static void main(String[] args){
-        PingPong ping = new PingPong("PING", 33);
-        PingPong pong = new PingPong("PONG", 10);
-        PingPong pang = new PingPong("PANG", 20);
-        PingPong pung = new PingPong("PUNG", 25);
-        
+        PingPong[] ping = new PingPong[4];
+        for(int x = 0; x < 4; x++){
+            ping[x] = new PingPong("x"+x, x*13);
+        }
+//        PingPong ping = new PingPong("PING", 33);
+//        PingPong pong = new PingPong("PONG", 10);
+//        PingPong pang = new PingPong("PANG", 20);
+//        PingPong pung = new PingPong("PUNG", 25);
+//        
         //Creacion de hilos
-        Thread t1 = new Thread(ping);
-        Thread t2 = new Thread(pong);
-        Thread t3 = new Thread(pang);
-        Thread t4 = new Thread(pung);
+        Thread[] misHilos = new Thread[4];
+        for(int i = 0; i < 4; i++){
+            misHilos[i] = new Thread(ping[i]);
+        }
+//        Thread t1 = new Thread(ping);
+//        Thread t2 = new Thread(pong);
+//        Thread t3 = new Thread(pang);
+//        Thread t4 = new Thread(pung);
         
         //Activacion de los hilos
-        t1.start();
-        t2.start();
-        t3.start();
-        t4.start();
+        for(int i = 4; i < 4; i++){
+            misHilos[i].start();
+        }
+//        t1.start();
+//        t2.start();
+//        t3.start();
+//        t4.start();
         
         //Espera unos segundos
         try{
