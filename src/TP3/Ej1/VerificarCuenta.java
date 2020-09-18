66c3e48c03dc852b14f3dcf9ba5c92bbe3d85023
Nicolás <Nicols@192.168.0.168>
2020-09-18 12:46:44 -0300

    Corregidos los sleep

--- src/TP3/Ej1/VerificarCuenta.java
+++ src/TP3/Ej1/VerificarCuenta.java
@@ -19,14 +19,14 @@ public class VerificarCuenta implements Runnable {
     private synchronized void HacerRetiro(int cantidad) throws InterruptedException { //sincronizado para evitar problemas de concurrencia
         if (cb.getBalance() >= cantidad) {
             System.out.println(Thread.currentThread().getName() + " está realizando un retiro de: " + cantidad + ".");
-//            Thread.sleep(1000);
+            Thread.sleep(1000);
             cb.retiroBancario(cantidad);
             System.out.println(Thread.currentThread().getName() + ": Retiro realizado");
             System.out.println(Thread.currentThread().getName() + ": Los fondos son de: " + cb.getBalance());
         } else {
             System.out.println("No hay suficiente dinero en la cuenta para realizar el retiro Sr." + Thread.currentThread().getName());
             System.out.println("Su saldo actual es de: " + cb.getBalance());
-//            Thread.sleep(1000);
+            Thread.sleep(1000);
         }
     } //de hacer retiro
 
@@ -37,7 +37,7 @@ public class VerificarCuenta implements Runnable {
                 if (cb.getBalance() < 0) {
                     System.out.println("La cuenta está sobregirada.");
                 }
-                Thread.sleep(500); //Se realiza un retiro y se le da el lock a otro hilo mientras este duerme
+//                Thread.sleep(500); //Se realiza un retiro y se le da el lock a otro hilo mientras este duerme
             } catch (InterruptedException ex) {
                 Logger.getLogger(VerificarCuenta.class.getName()).log(Level.SEVERE, null, ex);
             }
