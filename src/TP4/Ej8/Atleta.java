8d6353f45dd35c58432637e15ad31196f401583e
Nicolás <Nicols@192.168.0.168>
2020-09-25 16:16:44 -0300

    v1.1

--- src/TP4/Ej8/Atleta.java
+++ src/TP4/Ej8/Atleta.java
@@ -14,19 +14,6 @@ import java.util.logging.Logger;
  */
 public class Atleta implements Runnable {
 
-    //COLORES
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-
-    public static final String ANSI_RESET = "\u001B[0m";
-
-    //COLORES
     private Testigo testigo;
     int numeroAleatorio;
 
@@ -34,61 +21,25 @@ public class Atleta implements Runnable {
         testigo = test;
     }
 
-    public void correrAt1() throws InterruptedException {
-        System.out.println(ANSI_RED + "Atleta 1" + ANSI_RESET + " empieza a correr!");
-        numeroAleatorio = (int) (Math.random() * 3 + 1) + 8;
-        Thread.sleep(numeroAleatorio * 1000);
-        System.out.println(ANSI_RED + "Atleta 1" + ANSI_RESET + " corrio " + numeroAleatorio + " segundos!");
-        testigo.sem1.release();
-    }
-
-    private void correrAt2() throws InterruptedException {
-        testigo.sem1.acquire();
-        System.out.println(ANSI_GREEN + "Atleta 2" + ANSI_RESET + " empieza a correr!");
-        numeroAleatorio = (int) (Math.random() * 3 + 1) + 8;
-        Thread.sleep(numeroAleatorio * 1000);
-        System.out.println(ANSI_GREEN + "Atleta 2" + ANSI_RESET + " corrio " + numeroAleatorio + " segundos!");
-        testigo.sem2.release();
-    }
-
-    private void correrAt3() throws InterruptedException {
-        testigo.sem2.acquire();
-        System.out.println(ANSI_BLUE + "Atleta 3" + ANSI_RESET + " empieza a correr!");
-        numeroAleatorio = (int) (Math.random() * 3 + 1) + 8;
-        Thread.sleep(numeroAleatorio * 1000);
-        System.out.println(ANSI_BLUE + "Atleta 3" + ANSI_RESET + " corrio " + numeroAleatorio + " segundos!");
-        testigo.sem3.release();
-    }
-
-    private void correrAt4() throws InterruptedException {
-        testigo.sem3.acquire();
-        System.out.println(ANSI_PURPLE + "Atleta 4" + ANSI_RESET + " empieza a correr!");
-        numeroAleatorio = (int) (Math.random() * 3 + 1) + 8;
-        Thread.sleep(numeroAleatorio * 1000);
-        System.out.println(ANSI_PURPLE + "Atleta 4" + ANSI_RESET + " corrio " + numeroAleatorio + " segundos!");
-    }
-
     @Override
     public void run() {
-
         try {
             switch (Thread.currentThread().getName()) {
                 case "Atleta 1":
-                    correrAt1();
+                    testigo.correrAt1();
                     break;
                 case "Atleta 2":
-                    correrAt2();
+                    testigo.correrAt2();
                     break;
                 case "Atleta 3":
-                    correrAt3();
+                    testigo.correrAt3();
                     break;
                 case "Atleta 4":
-                    correrAt4();
+                    testigo.correrAt4();
                     break;
             }
         } catch (InterruptedException ex) {
             Logger.getLogger(Atleta.class.getName()).log(Level.SEVERE, null, ex);
-
         }
     }
 }
--- src/TP4/Ej8/Testigo.java
+++ src/TP4/Ej8/Testigo.java
@@ -13,12 +13,58 @@ import java.util.concurrent.Semaphore;
  */
 public class Testigo {
 
-    public Semaphore sem1, sem2, sem3;
-    
+    //COLORES
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    //COLORES
+    private Semaphore semA, semB, semC;
+    int numeroAleatorio;
+
     public Testigo() {
-        sem1 = new Semaphore(0);
-        sem2 = new Semaphore(0);
-        sem3 = new Semaphore(0);
+        semA = new Semaphore(0);
+        semB = new Semaphore(0);
+        semC = new Semaphore(0);
     }
-    
+
+    public void correrAt1() throws InterruptedException {
+        System.out.println(ANSI_RED + "Atleta 1" + ANSI_RESET + " empieza a correr!");
+        numeroAleatorio = (int) (Math.random() * 3 + 1) + 8;
+        Thread.sleep(numeroAleatorio * 1000);
+        System.out.println(ANSI_RED + "Atleta 1" + ANSI_RESET + " corrio " + numeroAleatorio + " segundos!");
+        semA.release();
+    }
+
+    public void correrAt2() throws InterruptedException {
+        semA.acquire();
+        System.out.println(ANSI_GREEN + "Atleta 2" + ANSI_RESET + " empieza a correr!");
+        numeroAleatorio = (int) (Math.random() * 3 + 1) + 8;
+        Thread.sleep(numeroAleatorio * 1000);
+        System.out.println(ANSI_GREEN + "Atleta 2" + ANSI_RESET + " corrio " + numeroAleatorio + " segundos!");
+        semB.release();
+    }
+
+    public void correrAt3() throws InterruptedException {
+        semB.acquire();
+        System.out.println(ANSI_BLUE + "Atleta 3" + ANSI_RESET + " empieza a correr!");
+        numeroAleatorio = (int) (Math.random() * 3 + 1) + 8;
+        Thread.sleep(numeroAleatorio * 1000);
+        System.out.println(ANSI_BLUE + "Atleta 3" + ANSI_RESET + " corrio " + numeroAleatorio + " segundos!");
+        semC.release();
+    }
+
+    public void correrAt4() throws InterruptedException {
+        semC.acquire();
+        System.out.println(ANSI_PURPLE + "Atleta 4" + ANSI_RESET + " empieza a correr!");
+        numeroAleatorio = (int) (Math.random() * 3 + 1) + 8;
+        Thread.sleep(numeroAleatorio * 1000);
+        System.out.println(ANSI_PURPLE + "Atleta 4" + ANSI_RESET + " corrio " + numeroAleatorio + " segundos!");
+    }
 }
